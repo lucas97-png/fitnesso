96fa623174c33f08b6d978d5b68cbaf99a40e953
lucas97-png <74870937+lucas97-png@users.noreply.github.com>
2021-10-28 22:46:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.querySelector('.toggler').onclick = () => {
     document.querySelector('ul.navigation').classList.toggle('active');
 }
 
-window.onscroll = () => {
+window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('ul.navigation').classList.remove('active');
-}
+});
